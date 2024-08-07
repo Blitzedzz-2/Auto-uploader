e08957ea7f3b17250b36f7e3ff7007e046b88b7a
Blitzedzz <blitzedzzz@proton.me>
2024-08-06 20:48:21 -0400

    Add files via upload

---
+++ start.bat
@@ -0,0 +1,18 @@
+echo off
+cls
+python -m pip install pyautogui
+python -m pip install pyperclip
+python -m pip install requests
+python -m pip install pygetwindow
+python3.10 -m pip install pyautogui
+python3.10 -m pip install pyperclip
+python3.10 -m pip install requests
+python3.10 -m pip install pygetwindow
+
+echo Made by blitzedzz frfr
+title Made by blitzedzz ^| Uploader
+:s
+python AutoUploader.py
+python3.10 AutoUploader.py
+
+goto s
